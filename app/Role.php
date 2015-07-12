f102403ddcff991256a55ab91daff16dd755c3c2
arnold <arnold@arnold-pc>
2015-07-12 16:51:59 +0300

    before 1 push

--- app/Role.php
+++ app/Role.php
@@ -4,12 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model {
-
-    protected $table = 'roles';
-
-    public function users() {
-        return $this->belongsToMany('App\User');
-    }
-
+class Role extends Model
+{
+    //
 }
