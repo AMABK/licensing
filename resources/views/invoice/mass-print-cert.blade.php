d807e5cc780efe1449e81084497b9c9da61e45f6
arnold <arnold@arnold-pc>
2015-08-07 13:04:25 +0300

    Commit by Arnold, @Prisk in the morning

--- resources/views/invoice/mass-print-cert.blade.php
+++ resources/views/invoice/mass-print-cert.blade.php
@@ -17,7 +17,7 @@ ob_start();
         barcode{
             position:absolute;
             margin-left: -39.55%;
-            padding-top: 47px;
+            padding-top: 43px;
         }
         img{
             position:absolute;
